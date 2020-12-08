c946f87e0b24e2cb0738424be8ed1a69a867ba0a
y724u <68385107+y724u@users.noreply.github.com>
2020-12-08 21:12:42 +0900

    モーダルを追加

---
+++ js/script.js
@@ -0,0 +1,22 @@
+var swiper = new Swiper('.swiper-container', {
+
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  speed: 500,
+});
+
+// // <!-- モーダル -->
+$(function() {
+  $('.js-modalOpen').on('click',function(){
+    $('.slide__modal').addClass('show_modal');
+  });
+  $('.js-modalClose').on('click',function(){
+    $('.slide__modal').removeClass('show_modal');
+  });
+});
