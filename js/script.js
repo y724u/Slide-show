a96d658d2067c1840dbd6ea212a69d06edc2dd68
y724u <68385107+y724u@users.noreply.github.com>
2020-12-09 16:01:38 +0900

    画像をクリック表示

--- js/script.js
+++ js/script.js
@@ -1,23 +1,29 @@
-var swiper = new Swiper('.swiper-container', {
-
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'fraction',
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  speed: 500,
-});
-
-// // <!-- モーダル -->
-$(function() {
-  $('.js-modalOpen').on('click',function(){
+// <!-- モーダル -->
+$(function () {
+  $('.js-modalOpen').on('click', function () {
+    const $index = $(this).index();
     $('.slide__modal').addClass('show_modal');
+    showImage($index);
   });
-  $('.js-modalClose').on('click',function(){
+  $('.js-modalClose').on('click', function () {
     $('.slide__modal').removeClass('show_modal');
   });
 });
+
+// <!-- swiper -->
+function showImage($index) {
+  var swiper = new Swiper('.swiper-container', {
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    speed: 500,
+    initialSlide: $index,
+  });
+}
+
 
