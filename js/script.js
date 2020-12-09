a071d0efb2b83663367d6d1fcb117d50d07e0e89
y724u <68385107+y724u@users.noreply.github.com>
2020-12-09 16:50:00 +0900

    css , js を修正

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
 // <!-- モーダル -->
 $(function () {
+  let scrollPosition;
   $('.js-modalOpen').on('click', function () {
-    const $index = $(this).index();
+    const $this = $(this);
+    const $index = $this.index();
     $('.slide__modal').addClass('show_modal');
     showImage($index);
+    scrollPosition = $(window).scrollTop();
+    $('body').addClass('fixed').css({ 'top': -scrollPosition1 });
+    return false;
   });
   $('.js-modalClose').on('click', function () {
     $('.slide__modal').removeClass('show_modal');
+    $('body').removeClass('fixed').css({ 'top': 0 });
+    window.scrollTo(0, scrollPosition);
   });
 });
 
